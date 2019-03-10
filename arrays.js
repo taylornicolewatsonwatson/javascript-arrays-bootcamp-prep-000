b443c12f41528c08ff028e104a030f80b2c68377
Taylor Watson <taylortwatson@gmail.com>
2019-03-10 01:35:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,8 @@ function addElementToBeginningOfArray(array,element){ return [element, ...array]
 function destructivelyAddElementToBeginningOfArray(array,element){ array.unshift(element); return array }
 function  addElementToEndOfArray(array,element){ return [ array, ...element] }
 function destructivelyAddElementToEndOfArray(array, element){ array.push(element); return array }
-function accessElementInArray(array,index){ return
+function accessElementInArray(array,index){ return array[index] }
+function destructivelyRemoveElementFromBeginningOfArray(array){ array.shift(element); return array }
+function removeELementFromBeginningOfArray(array){ array.slice(1); return array }
+function destructivelyRemoveElementFromEndOfArray(array){
+function removeElementFromEndOfArray(array)
