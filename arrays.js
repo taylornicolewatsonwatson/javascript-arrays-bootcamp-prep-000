3171780a421075f620bcb59cd52066ec0b934b7e
Taylor Watson <taylortwatson@gmail.com>
2019-03-10 01:29:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
 function addElementToBeginningOfArray(array,element){ return [element, ...array] }
 function destructivelyAddElementToBeginningOfArray(array,element){ array.unshift(element); return array }
 function  addElementToEndOfArray(array,element){ return [ array, ...element] }
-function destrucitvelyAddElementToEndOfArray(array,element){}
+function destrucitvelyAddElementToEndOfArray(array,element){ array.push(element); return array }
+
